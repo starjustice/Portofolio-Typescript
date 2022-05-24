3d6f4b3005c7de064a1da6d3ff92e7b0148c7369
Switzer <stephenmulyadi97@gmail.com>
2022-05-24 20:32:52 +0700

    setup

---
+++ src/general/styles.ts
@@ -0,0 +1,18 @@
+export let COLORS = {
+  white: "#fff",
+  black: "#020202",
+  darkGreen: "#43a047",
+  teal: "#e0f2f1",
+  darkIndigo: "#1a237e",
+  indigo: "#c5cae9",
+  darkPurple: "#4a148c",
+};
+
+export let FONTS_SIZE = {
+  small: 16,
+  medium: 20,
+  large: 28,
+  xLarge: 36,
+  xxLarge: 48,
+  jumbo: 60,
+};
---
+++ src/scene/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "../core-ui";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Text style={{ color: "red" }}>ada</Text>
+    </div>
+  );
+}
