cc64c63e546c31f48bdc7d416922c23f760c6a60
Switzer <stephenmulyadi97@gmail.com>
2022-05-24 20:27:46 +0700

    add image

---
+++ src/images/index.ts
@@ -0,0 +1,6 @@
+import MyImage from "./selfPhoto.jpg";
+import HuddleFire from "./huddleFire.png";
+import Insight from "./insights_logo.png";
+import DBO from "./tokoLogo.png";
+
+export { MyImage, HuddleFire, Insight, DBO };
