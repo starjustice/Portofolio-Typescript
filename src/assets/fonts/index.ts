6132fd56cf512771bdff329cd18dd23afe32926e
Switzer <stephenmulyadi97@gmail.com>
2022-06-03 19:36:01 +0700

    add new background header and text

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,6 @@
+import Hallelujah from "./gloria_hallelujah/gloriahallelujah.ttf";
+import MadeTommy from "./made_tommy/MADE TOMMY Regular_PERSONAL USE.otf";
+import MadeTommyBold from "./made_tommy/MADE TOMMY Bold_PERSONAL USE.otf";
+import MadeTommyMedium from "./made_tommy/MADE TOMMY Medium_PERSONAL USE.otf";
+
+export { Hallelujah, MadeTommy, MadeTommyBold, MadeTommyMedium };
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,6 @@ import Insight from "./insights_logo.png";
 import DBO from "./tokoLogo.png";
 import Paint from "./paint.webp";
 import Code from "./code.jpeg";
+import Computer from "./computer.jpg";
 
-export { MyImage, HuddleFire, Insight, DBO, Paint, Code };
+export { MyImage, HuddleFire, Insight, DBO, Paint, Code, Computer };
