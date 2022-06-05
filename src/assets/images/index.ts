e6c16659a9c0b00cae059a00cf8f749094591ac8
Switzer <stephenmulyadi97@gmail.com>
2022-06-05 22:20:00 +0700

    image webp

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,6 +4,6 @@ import Insight from "./insights_logo.png";
 import DBO from "./tokoLogo.png";
 import Paint from "./paint.webp";
 import Code from "./code.jpeg";
-import Computer from "./computer.jpg";
+import Computer from "./computer.webp";
 
 export { MyImage, HuddleFire, Insight, DBO, Paint, Code, Computer };
